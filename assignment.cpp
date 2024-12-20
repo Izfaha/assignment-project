f62cdcebc566a75b38212c8c3967b9150afe903c
Izfaha <rasberry006@gmail.com>
2024-12-20 20:19:49 +0700

    last change

--- assignment.cpp
+++ assignment.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
-struct nilai
-{
+struct nilai {
     int nil_mhs;
     string nama_mhs;
 };
 
-void data_mhs(){
-    int nilai_raport_mhs[4]={12, 22, 43, 55};
-    cout << nilai_raport_mhs[4] << endl;
+void data_mhs() {
+    int nilai_raport_mhs[4] = {12, 22, 43, 55};
+
+    // Loop through and print the values of the array
+    for (int i = 0; i < 4; i++) {
+        cout << "Nilai ke-" << i + 1 << ": " << nilai_raport_mhs[i] << endl;
+    }
 }
 
-int main(){
-    cout 
+int main() {
+    cout << "Report Nilai Mahasiswa : " << endl;
     data_mhs();
     return 0;
 }
