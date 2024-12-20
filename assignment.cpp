aa0cf8a72d526dfb902de91166077b2616176217
Izfaha <rasberry006@gmail.com>
2024-12-20 19:09:29 +0700

    Assigment for daspro project

---
+++ assignment.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct nilai
+{
+    int nil_mhs;
+    string nama_mhs;
+};
+
+void data_mhs(){
+    nilai nil_mhs[2][2]={};
+}
+
+int main(){
+
+    return 0;
+}
